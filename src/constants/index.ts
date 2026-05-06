18b396488f3d91cf03232f813bc5d765b9e72be5
Gustavo Henrique <gustavopp.dev@gmail.com>
2026-05-06 15:58:57 -0300

    feat(navbar): navbar mobile e desktop (MVP)

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+import { NavigationLinkType } from "@/types";
+
+export const NavigationLinks: NavigationLinkType[] = [
+  { href: "/", label: "newArrivals" },
+  { href: "/", label: "women" },
+  { href: "/", label: "men" },
+  { href: "/", label: "accessories" },
+  { href: "/", label: "collections" },
+  { href: "/", label: "sale" },
+];
