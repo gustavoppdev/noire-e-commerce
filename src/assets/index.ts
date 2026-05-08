5bd9f1820551a0febd3c026735b0959663dd97ff
Gustavo Henrique <gustavopp.dev@gmail.com>
2026-05-08 16:01:53 -0300

    feat(about): adiciona a section about

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import heroImg from "./images/hero.webp";
 import productTestImg from "./images/product-test.webp";
+import aboutImg from "./images/about.webp";
 
-export { heroImg, productTestImg };
+export { heroImg, productTestImg, aboutImg };
